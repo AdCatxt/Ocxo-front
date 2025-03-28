0bad6a0da3909d1dc34f4d38c499590345ba7525
AdCatxt <adcatzt@gmail.com>
2025-03-28 06:16:04 -0600

    Feat: JWT retrieved

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,22 +1,36 @@
+'use client';
 import { Button, Input } from "@heroui/react";
 import { Link } from '@heroui/react'
+import { API_URL } from "@/constants";
+import axios from "axios";
+const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault()
+    const formData = new FormData(e.target);
+    let authData: any = {}
+    authData.userEmail = formData.get('userEmail');
+    authData.userPassword = formData.get('userPassword');
+    const { data } = await axios.post(`${API_URL}/auth/login`, {
+        ...authData
+    });
+    console.log(authData)
+    return
+}
 
 export default function LoginPage() {
     return (
-        <div className='bg-yellow-400 px-10 py-2 rounded-md'>
+        <form className='bg-yellow-400 px-10 py-2 rounded-md' onSubmit={handleSubmit}>
             <p className='text-2xl my-4 text-white'>Inicio de Sesión</p>
             <div className='flex flex-col gap-2 my-4'>
-                < Input label='Email' type='email' isRequired={true} size='sm' />
-                < Input label='Contraseña' type='password' isRequired={true} size='sm' />
-                < Input label='Confirmar Contraseña' type='password' isRequired={true} size='sm' />
+                < Input label='Email' name='userEmail' type='email' isRequired={true} size='sm' />
+                < Input label='Contraseña' name='userPassword' type='password' isRequired={true} size='sm' />
             </div>
             <div className='flex flex-col items-center gap-2'>
-                <Button color='primary'>Iniciar Sesión</Button>
+                <Button color='primary' type='submit'>Iniciar Sesión</Button>
                 <div className='flex items-center gap-1'>
-                <p className='text-white'>¿No tienes cuenta?</p>
-                <Link href='/singup' className='underline'>Registrate</Link>
+                    <p className='text-white'>¿No tienes cuenta?</p>
+                    <Link href='/singup' className='underline'>Registrate</Link>
                 </div>
             </div>
-        </div>
+        </form>
     )
 }
--- app/dashboard/@count/page.tsx
+++ app/dashboard/@count/page.tsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const CountPage = async () => {
-    const countLocations = await axios.get('http://localhost:3000/locations')
-    return 'Hay ' + countLocations?.data?.length + 'locations';
+    const countLocations = await axios.get('http://127.0.0.1:4000/locations')
+    return 'Hay ' + countLocations?.data?.length + ' locaciones';
 }
 
 export default CountPage;
