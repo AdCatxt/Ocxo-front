e0fa0fd97636f0fc1360d4e1b5ed7bc6b077f9a5
AdCatxt <adcatzt@gmail.com>
2025-03-30 15:33:18 -0600

    Muchas cosas, tarea 11 por fin quedó

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -3,20 +3,24 @@ import { Button, Input } from "@heroui/react";
 import { Link } from '@heroui/react'
 import { API_URL } from "@/constants";
 import axios from "axios";
-const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault()
-    const formData = new FormData(e.target);
-    let authData: any = {}
-    authData.userEmail = formData.get('userEmail');
-    authData.userPassword = formData.get('userPassword');
-    const { data } = await axios.post(`${API_URL}/auth/login`, {
-        ...authData
-    });
-    console.log(authData)
-    return
-}
 
 export default function LoginPage() {
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault()
+        const formData = new FormData(e.target);
+        let authData: any = {}
+        authData.userEmail = formData.get('userEmail');
+        authData.userPassword = formData.get('userPassword');
+        const { data } = await axios.post(`${API_URL}/auth/login`, {
+            ...authData
+        }, {
+            withCredentials: true,
+
+        });
+        console.log(authData)
+        return
+    }
+
     return (
         <form className='bg-yellow-400 px-10 py-2 rounded-md' onSubmit={handleSubmit}>
             <p className='text-2xl my-4 text-white'>Inicio de Sesión</p>
